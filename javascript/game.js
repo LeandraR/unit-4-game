5c9e609adcfca1824e55e85bee2593a9b2e4d9ac
leandra <leandra.reid@gmail.com>
2018-05-03 17:10:52 -0400

    mostly finished

--- javascript/game.js
+++ javascript/game.js
@@ -1,30 +1,58 @@
 $(document).ready (function (){
+    var scoreCounter = 0;
+    var wins = 0;
 
-    var numberOptions = [10, 5, 3, 1];
+    // start button on click, game numbers reset
+    $('#start').on('click', function(){
 
-// on click 'try again button', game numbers reset
-    $('#tryAgain').on('click', function(){
         var randomNumber = Math.floor(Math.random()*100);
         $('.gameScore').html(randomNumber);
 
-    //assign values to crystals
-        for (var i = 0; i < numberOptions.length; i++) {
-            $('.crystal-image').attr("data-crystalvalue", numberOptions[i]);
-        }
+        //generate value of each crystal per game
+        var crystalOne = Math.floor((Math.random() * 10) + 1);
+        var crystalTwo = Math.floor((Math.random() * 10) + 1);
+        var crystalThree = Math.floor((Math.random() * 10) + 1);
+        var crystalFour = Math.floor((Math.random() * 10) + 1);
 
-        $('.crystal-image').on('click', function() {
-            var crystalValue = ($(this).attr("data-crystalvalue"));
-            crystalValue = parseInt(crystalValue);
+        function scoreCheck()
+        {
+           if (scoreCounter === randomNumber){
+                    alert("You have placated the Ancient Dieties... this time");
+                    $('.winCounter').html(wins++);
+                    $('.scoreCounter').html(scoreCounter);
+            } else if (scoreCounter > randomNumber){
+                    alert("You are doomed to be trapped in the Eternal Palace forever...");
+                    $('.scoreCounter').html(scoreCounter);
+            }
+        };
+        //on click of each crystal, add value
+        $('#crystalOne').click(function() {
+            console.log(crystalOne);
+            $('.scoreCounter').html(scoreCounter+=crystalOne);
+            scoreCheck();
+        });
+
+        $('#crystalTwo').click(function() {
+            console.log(crystalTwo);
+            $('.scoreCounter').html(scoreCounter+=crystalTwo);
+            scoreCheck();
+        });
+
+        $('#crystalThree').click(function() {
+            console.log(crystalThree);
+            $('.scoreCounter').html(scoreCounter+=crystalThree);
+            scoreCheck();
+        });
+
+        $('#crystalFour').click(function() {
+            console.log(crystalFour);
+            $('.scoreCounter').html(scoreCounter+=crystalFour);
+            scoreCheck();
+        });
+
+
+//win counter not updating properly - fix
+// score re-set to zero on click again
 
     });
-
-});
-
-
-
-
-
-
-
-
 });
